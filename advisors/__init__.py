59b67a60ba53515f2ef442894e3d59ef98a53a1a
Vadim Chizhov <chizhov.vadim@gmail.com>
2025-07-02 09:55:34 +0300

    None values in WeightSumIndicator

--- advisors/__init__.py
+++ advisors/__init__.py
@@ -3,6 +3,13 @@ from .domaintypes import Advisor
 
 
 def buildAdvisor(name: str):
+    res = _makeAdvisor(name)
+    if res is None:
+        raise ValueError("bad advisor name", name)
+    return res
+
+
+def _makeAdvisor(name: str):
     if name.startswith("sample_"):
         name = name.removeprefix("sample_")
         return sample.buildAdvisor(name)
--- advisors/sample.py
+++ advisors/sample.py
@@ -58,19 +58,30 @@ class EmaIndicator:
 
 class WeightSumIndicator:
     def __init__(self, childs, weights: list[float]):
+        if weights is None:
+            weights = [1.0/len(childs) for _ in childs]
+        elif len(childs) != len(weights):
+            raise ValueError("wrong size WeightSumIndicator")
         self._childs = childs
         self._weights = weights
+        self._values = [0.0 for _ in childs]
 
     def add(self, dt, v):
         for child in self._childs:
             child.add(dt, v)
 
     def value(self):
-        sum = 0.0
+        hasValue = False
         for i, child in enumerate(self._childs):
             childValue = child.value()
             if childValue is not None:
-                sum += self._weights[i] * childValue
+                hasValue = True
+                self._values[i] = childValue
+        if not hasValue:
+            return None
+        sum = 0.0
+        for w, v in zip(self._weights, self._values):
+            sum += w * v
         return max(-1, min(1, sum))
 
 
